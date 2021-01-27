f3501a688ff9d35d77b9c5590aabbf91b6c1a116
Zane van Iperen <zane@zanevaniperen.com>
2021-01-27 22:00:59 +1000

    vsclib: add vsc_strrdup{,a}()

---
+++ vsclib/string.c
@@ -0,0 +1,44 @@
+/*
+ * vsclib
+ * https://{github.com,codeberg.org}/vs49688/vsclib
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021 Zane van Iperen (zane@zanevaniperen.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <assert.h>
+#include "vsclib_i.h"
+
+char *vsc_strrdupa(const char *start, const char *end, const vsc_allocator_t *a)
+{
+    char *buf;
+    size_t len;
+    assert(start != NULL && end != NULL && end >= start && a != NULL);
+
+    len = (size_t)(end - start);
+    if((buf = vsci_xalloc(a, (len + 1) * sizeof(char))) == NULL) {
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    memcpy(buf, start, len);
+    buf[len] = '\0';
+    return buf;
+}
+
+
+char *vsc_strrdup(const char *start, const char *end)
+{
+    return vsc_strrdupa(start, end, &vsclib_system_allocator);
+}
