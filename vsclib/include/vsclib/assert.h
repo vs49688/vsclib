4dd487baa26e6a02bff4f0fbd829b500b030a4a6
Zane van Iperen <zane@zanevaniperen.com>
2021-05-10 21:20:59 +1000

    vsclib: add vsc_assert() macro
    Is just #define'd to assert() for now. Will implement something
    proper later.

---
+++ vsclib/include/vsclib/assert.h
@@ -0,0 +1,32 @@
+/*
+ * vsclib
+ * https://{github.com,codeberg.org}/vs49688/vsclib
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021 Zane van Iperen (zane@zanevaniperen.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _VSCLIB_ASSERT_H
+#define _VSCLIB_ASSERT_H
+
+#include <assert.h>
+#include "vsclib_config.h"
+
+#if defined(VSC_DEBUG)
+#   define vsc_assert(x) assert(x)
+#else
+#   define vsc_assert(x)
+#endif
+
+#endif /* _VSCLIB_ASSERT_H */
