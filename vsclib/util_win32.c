527053e4f141d92f7e11b82bea88a71cf8357080
Zane van Iperen <zane@zanevaniperen.com>
2021-10-09 14:03:24 +1100

    vsclib/util_win32: add

---
+++ vsclib/util_win32.c
@@ -0,0 +1,44 @@
+/*
+ * vsclib
+ * https://{github.com,codeberg.org}/vs49688/vsclib
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021 Zane van Iperen (zane@zanevaniperen.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifdef _WIN32
+
+#include <vsclib/error.h>
+#include "util_win32.h"
+
+int vsci_map_win32err(DWORD dwErr)
+{
+    switch(dwErr) {
+        case ERROR_FILE_NOT_FOUND:
+        case ERROR_PATH_NOT_FOUND:
+            return VSC_ERROR(ENOENT);
+        case ERROR_PATH_BUSY:
+            return VSC_ERROR(EBUSY);
+        case ERROR_ACCESS_DENIED:
+            return VSC_ERROR(EACCES);
+        case ERROR_INSUFFICIENT_BUFFER:
+        case ERROR_INVALID_FLAGS:
+        case ERROR_INVALID_PARAMETER:
+        case ERROR_NO_UNICODE_TRANSLATION:
+        default:
+            return VSC_ERROR(EINVAL);
+    }
+}
+
+#endif
---
+++ vsclib/util_win32.h
@@ -0,0 +1,28 @@
+/*
+ * vsclib
+ * https://{github.com,codeberg.org}/vs49688/vsclib
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021 Zane van Iperen (zane@zanevaniperen.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#if !defined(_VSCLIB_UTIL_WIN32_H) && defined(_WIN32)
+#define _VSCLIB_UTIL_WIN32_H
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int vsci_map_win32err(DWORD dwErr);
+
+#endif /* _VSCLIB_UTIL_WIN32_H */
