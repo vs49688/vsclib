76f0994ddfaf6c0f7ec0f91708190681e51de6b0
Zane van Iperen <zane@zanevaniperen.com>
2022-07-25 01:00:29 +1000

    flake: add musl and win64 targets

--- flake.nix
+++ flake.nix
@@ -9,7 +9,21 @@
       default = vsclib;
     };
   in {
-    packages.x86_64-linux   = mkPackages { system = "x86_64-linux";   };
+    packages.x86_64-linux   = mkPackages { system = "x86_64-linux"; } // {
+      vsclib-musl = let
+        musl64Packages = import nixpkgs {
+          system = "x86_64-linux";
+          crossSystem = nixpkgs.lib.systems.examples.musl64;
+        };
+      in musl64Packages.callPackage ./default.nix { version = self.lastModifiedDate; };
+
+      vsclib-win64 = let
+        win64Packages = import nixpkgs {
+          system = "x86_64-linux";
+          crossSystem = nixpkgs.lib.systems.examples.mingwW64;
+        };
+      in win64Packages.callPackage ./default.nix { version = self.lastModifiedDate; };
+    };
     packages.aarch64-linux  = mkPackages { system = "aarch64-linux";  };
     packages.x86_64-darwin  = mkPackages { system = "x86_64-darwin";  };
     packages.aarch64-darwin = mkPackages { system = "aarch64-darwin"; };
