392bc1e51ce3f7d5da8d271e0fab4997cd57eab8
Zane van Iperen <zane@zanevaniperen.com>
2022-12-04 23:46:59 +1000

    nix: refactor flake

--- flake.nix
+++ flake.nix
@@ -7,37 +7,36 @@
     in rec {
       vsclib  = pkgs.callPackage ./default.nix { version = self.lastModifiedDate; };
       default = vsclib;
-    };
-  in {
-    packages.x86_64-linux   = mkPackages { system = "x86_64-linux"; } // {
-      vsclib-musl = let
-        musl64Packages = import nixpkgs {
-          system = "x86_64-linux";
-          crossSystem = nixpkgs.lib.systems.examples.musl64;
-        };
-      in musl64Packages.callPackage ./default.nix { version = self.lastModifiedDate; };
 
-      vsclib-win64 = let
-        win64Packages = import nixpkgs {
-          system = "x86_64-linux";
-          crossSystem = nixpkgs.lib.systems.examples.mingwW64;
-        };
-      in win64Packages.callPackage ./default.nix { version = self.lastModifiedDate; };
+      vsclib-clang = vsclib.override { stdenv = pkgs.clangStdenv; };
     };
+
+    mkCross = { crossSystem }: let
+      crossPackages = import nixpkgs {
+        inherit crossSystem;
+        system = "x86_64-linux";
+      };
+    in crossPackages.callPackage ./default.nix { version = self.lastModifiedDate; };
+
+    mkShells = packages: builtins.mapAttrs (k: v: v.overrideAttrs(old: {
+      hardeningDisable = [ "all" ];
+    })) packages;
+
+  in {
+    packages.x86_64-linux = mkPackages { system = "x86_64-linux"; } // {
+      vsclib-musl    = mkCross { crossSystem = nixpkgs.lib.systems.examples.musl64;   };
+      vsclib-win32   = mkCross { crossSystem = nixpkgs.lib.systems.examples.mingw32;  };
+      vsclib-win64   = mkCross { crossSystem = nixpkgs.lib.systems.examples.mingwW64; };
+      vsclib-aarch64 = mkCross { crossSystem = nixpkgs.lib.systems.examples.aarch64-multiplatform; };
+    };
+
     packages.aarch64-linux  = mkPackages { system = "aarch64-linux";  };
     packages.x86_64-darwin  = mkPackages { system = "x86_64-darwin";  };
     packages.aarch64-darwin = mkPackages { system = "aarch64-darwin"; };
 
-    devShells.x86_64-linux.default = self.packages.x86_64-linux.default.overrideAttrs(old: {
-      hardeningDisable = [ "all" ];
-    });
-
-    devShells.x86_64-darwin.default = self.packages.x86_64-darwin.default.overrideAttrs(old: {
-      hardeningDisable = [ "all" ];
-    });
-
-    devShells.aarch64-darwin.default = self.packages.aarch64-darwin.default.overrideAttrs(old: {
-      hardeningDisable = [ "all" ];
-    });
+    devShells.x86_64-linux   = mkShells self.packages.x86_64-linux;
+    devShells.aarch64-linux  = mkShells self.packages.aarch64-linux;
+    devShells.x86_64-darwin  = mkShells self.packages.x86_64-darwin;
+    devShells.aarch64-darwin = mkShells self.packages.x86_64-darwin;
   };
 }
