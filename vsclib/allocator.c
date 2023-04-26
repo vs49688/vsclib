81c08d2dffa050a17cdd5bb34e07f962c995b8d6
Zane van Iperen <zane@zanevaniperen.com>
2023-04-26 17:52:17 +1000

    vsclib: allocator: remove dependence on aligned_offset_malloc()
    Merge the appropriate logic into the allocator.

--- vsclib/allocator.c
+++ vsclib/allocator.c
@@ -19,15 +19,27 @@
  */
 
 /*
- * Basic aligned allocator implementation using a header to
- * store size information.
- * Takes advantage of _aligned_offset_realloc().
+ * Basic aligned allocator implementation using a header to store extra information.
  *
- * The size information is required in order to implement
- * VscAllocator::size. Without it, the only way to get the size of
- * an _aligned_offset_realloc()'d block is to use _aligned_msize(),
- * which requires saving the alignment, which would use the same
- * amount of storage anyway.
+ * The header should be at the start of the block.
+ *
+ * In most cases, i.e. the no special alignment requirements, the data starts
+ * immediately after the header. The header location can be verified by looking
+ * for a signature at the preceding `sizeof(void*)` bytes.
+ *
+ * In the case where the alignment requires excessive padding, this method
+ * won't work. The naive method is to scan backwards for the signature (slow).
+ * Instead, store a pointer to the header in the `sizeof(void*)` bytes, in the
+ * same place the signature would be and use that to locate the header.
+ *
+ * If these bytes match the signature, we know the header is immediately preceding
+ * the data. If they don't, assume it's a pointer to the header.
+ *
+ * TODO:
+ * - Add an optimisation to store the header as a footer when the alignment padding
+ *   would take up too much space. This still keeps the header address preceding the
+ *   data for easy lookup, but a flag would need to be stored in the header so we
+ *   don't try to free the wrong pointer.
  */
 #include <string.h>
 #include <vsclib/assert.h>
@@ -35,18 +47,31 @@
 #include <vsclib/error.h>
 #include <vsclib/mem.h>
 
-#include "mingw-aligned-malloc.h"
-
-#define MEMHDR_SIG 0xFEED5EED /* Formerly Chuck's */
+#define MEMHDR_SIG ((uintptr_t)0xFEED5EEDFEED5EEDu) /* Formerly Chuck's */
 
 typedef struct MemHeader {
-    size_t   size;
-    uint32_t sig;
+    size_t size;
+    union {
+        struct {
+            size_t align_power : 8;
+            size_t reserved : VSC_SIZE_T_BITSIZE - 8;
+        };
+        size_t _pad;
+    };
+    uintptr_t sig;
 } MemHeader;
 
 static MemHeader *mem2hdr(void *p)
 {
-    MemHeader *hdr = (MemHeader *)p - 1;
+    MemHeader *hdr  = NULL;
+    uintptr_t *pend = (uintptr_t *)VSC_ALIGN_DOWN(p, VSC_ALIGNOF(void *));
+
+    if(pend[-1] == MEMHDR_SIG)
+        hdr = (MemHeader *)pend - 1;
+    else
+        hdr = (MemHeader *)*(pend - 1);
+
+    vsc_assert(VSC_IS_ALIGNED(hdr, VSC_ALIGNOF(MemHeader)));
     vsc_assert(hdr->sig == MEMHDR_SIG);
     return hdr;
 }
@@ -56,9 +81,14 @@ static int malloc_(void **ptr, size_t size, size_t alignment, VscAllocFlags flag
     MemHeader *hdr = NULL, *nhdr;
     uint8_t   *p;
     size_t     reqsize, oldsize = 0;
+    uintptr_t  shift;
 
     (void)user;
     vsc_assert(VSC_IS_POT(alignment));
+
+    /* Enforce the minimum alignment, this will break headers otherwise. */
+    if(alignment < VSC_ALIGNOF(void *))
+        alignment = VSC_ALIGNOF(void *);
 
     /* Make everything easier for ourselves. */
     if(*ptr == NULL)
@@ -66,28 +96,53 @@ static int malloc_(void **ptr, size_t size, size_t alignment, VscAllocFlags flag
 
     vsc_assert(((flags & VSC_ALLOC_REALLOC) && *ptr != NULL) || (flags & VSC_ALLOC_REALLOC) == 0);
 
-    /* Size of the block + footer + alignment padding. */
-    reqsize = sizeof(MemHeader) + size;
+    /* Size of the header + block. */
+    reqsize = sizeof(MemHeader) + size + alignment;
 
     if(flags & VSC_ALLOC_REALLOC) {
         vsc_assert(*ptr != NULL);
         hdr     = mem2hdr(*ptr);
         oldsize = hdr->size;
+
+        /* If is an error for the alignment to decrease. */
+        if(alignment < (1u << hdr->align_power))
+            return VSC_ERROR(EINVAL);
+
+        shift = (uintptr_t)(*ptr) - (uintptr_t)hdr;
     }
 
-    p = vsci_aligned_offset_realloc(hdr, reqsize, alignment, sizeof(MemHeader));
-    if(p == NULL)
+    if((p = vsc_sys_realloc(hdr, reqsize)) == NULL)
         return VSC_ERROR(ENOMEM);
 
     nhdr = (MemHeader *)p;
-    p += sizeof(MemHeader);
+    p    = vsc_align_up(nhdr + 1, alignment);
 
     vsc_assert(VSC_IS_ALIGNED(p, alignment));
     vsc_assert(VSC_IS_ALIGNED(nhdr, VSC_ALIGNOF(MemHeader)));
 
-    // nhdr->size      = _aligned_msize(nhdr, alignment, sizeof(MemHeader)) - sizeof(MemHeader);
-    nhdr->size = size; /* NB: I've never seen this differ from the above. */
-    nhdr->sig  = MEMHDR_SIG;
+    /*
+     * For large alignments, store a pointer to the header immediately
+     * before our data, so we don't have to scan backwards for the header.
+     */
+    vsc_assert(VSC_IS_ALIGNED(p, VSC_ALIGNOF(void *)));
+    if((uintptr_t)(nhdr + 1) != (uintptr_t)p) {
+        vsc_assert((uintptr_t)p - (uintptr_t)(nhdr + 1) >= sizeof(void *));
+        *((void **)p - 1) = nhdr;
+    }
+
+    nhdr->size        = size;
+    nhdr->align_power = vsc_ctz(alignment);
+    nhdr->reserved    = 0;
+    nhdr->sig         = MEMHDR_SIG;
+
+    if(flags & VSC_ALLOC_REALLOC) {
+        /*
+         * If our alignment has increased, but we're still aligned our what we
+         * were before, the padding between the header/data may have changed. Account for this.
+         */
+        if(shift != ((uintptr_t)p - (uintptr_t)nhdr))
+            memmove(p, (void *)((uintptr_t)nhdr + shift), oldsize);
+    }
 
     if(flags & VSC_ALLOC_ZERO && nhdr->size > oldsize)
         memset(p + oldsize, 0, nhdr->size - oldsize);
@@ -102,7 +157,7 @@ static void free_(void *p, void *user)
     if(p == NULL)
         return;
 
-    vsci_aligned_free(mem2hdr(p));
+    vsc_sys_free(mem2hdr(p));
 }
 
 static size_t size_(void *p, void *user)
